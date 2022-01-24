6a70a86d438137b1f2038f01604a8819148fd95e
desady <89885409+demvbp@users.noreply.github.com>
2022-01-24 02:11:39 +0000

    Understanding elif Statements
    Key lesson: elif comes after if ALWAYS

---
+++ vampire.py
@@ -0,0 +1,10 @@
+# This name-checker program checks to see if a user is Alice or an imposter.
+
+if name == 'Alice':
+    print('Hi Alice.')
+elif age < 12:      # Alice is older than 12 years
+    print('You are not Alice, kiddo.')
+elif age > 2000:
+    print('Unlike you, Alice is not an undead, immortal vampire.')
+elif age > 100:
+    print('You are not Alice, grannie.')
