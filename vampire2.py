460437973be3d77d55a43cb38f7548d1b5a1a23d
desady <89885409+demvbp@users.noreply.github.com>
2022-02-04 00:54:57 +0000

    Update vampire2.py

--- vampire2.py
+++ vampire2.py
@@ -1,9 +1,11 @@
 # This name-checker program checks to see if a user is Alice or an imposter.
 # A bug has been introduced in this version to test the importance of order in elif Statements.
 
+name = 'Alice'
+age = 11
 if name == 'Alice':
     print('Hi Alice.')
-elif age < 12:      # Alice is older than 12 years
+elif age < 12:      # Alice is 12 years or older
     print('You are not Alice, kiddo.')
 elif age > 100:
     print('You are not Alice, grannie.')
