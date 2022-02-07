d1aeb5fb15d4699194fe3a0f4588ebafc9942cae
desady <89885409+demvbp@users.noreply.github.com>
2022-02-07 03:23:01 +0000

    Infinite Loop

---
+++ InfiniteLoop1.py
@@ -0,0 +1,6 @@
+spam = 0
+while spam < 5:
+    print('Hello, world.')
+    # spam = spam + 1 [Leaving this while statement at print() creates an infinite loop]
+# Press CTRL+C to stop the program immediately.
+# Pressing CTRL+C sends a KeyboardInterrupt error.
