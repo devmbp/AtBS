2fd6e916ae24098372434f86c170c532060ca488
desady <89885409+demvbp@users.noreply.github.com>
2022-02-07 03:26:20 +0000

    Iterations in Loop Statements

---
+++ yourName.py
@@ -0,0 +1,8 @@
+# An Annoying while Loop
+# Here's a program that will keep asking you to type your name over and over again
+
+name = ''
+while name != 'your name':
+    print('Please type your name.')
+    name = input()
+print('Thank you!')
