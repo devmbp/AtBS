30dc032f8a50a7a36230905ae5e6fd43185e21ba
desady <desady@icloud.com>
2022-02-19 20:54:31 +0000

    and, or Operators
    Once first condition is True, Python skips everything else.

---
+++ and or.py
@@ -0,0 +1,8 @@
+# Testing and, or operator evaluations
+x = 3
+y = 4
+
+print(x or y)
+
+# While both expressions are True, only the value of x is displayed
+# Because Python skips anything after the first True evaluation
