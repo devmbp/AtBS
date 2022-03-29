fee5b85059457a26ccb7344efb5ba07312b6af23
desady <desady@icloud.com>
2022-03-29 09:56:48 +0000

    Update FuelApp.py

--- FuelApp.py
+++ FuelApp.py
@@ -8,11 +8,17 @@ print('Welcome to GOIL FuelApp! Please enter the amount of fuel you wish to purc
 
 amountPurchased = input()   # Program takes user input here as str
 
-litresDispensed = int(amountPurchased) / pricePerLitre  # Python converts str input to int then Divides input by pricePerLitre
+litresDispensed = int(amountPurchased) / pricePerLitre  # Python converts str input to int then divides input by pricePerLitre
 
 litresDispensed = round(litresDispensed, 2) # round() rounds resultant float in line 10 to 2 decimal places
 
 print('Thank you for your purchase. You have been served' + ' ' + str(litresDispensed) + ' ' + 'litres of premium unleaded fuel.')
 # str() converts rounded value in line 15 to str
-# Concatenate all strs
-# Displays volume of fuel served to customer
+# print() concatenate all strs, displays volume of fuel served to customer
+
+print('To close this window, please type EXIT')
+
+close = input() # Ask user input 'EXIT'
+
+if close == 'EXIT': # if statement terminates program once user input = 'EXIT'
+    exit()
